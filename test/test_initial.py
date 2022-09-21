ffd2fd78a2cfe4c6ee86b3ce91e6523658e8900e
Richard Preen <rpreen@gmail.com>
2022-09-21 18:00:44 +0100

    update test

--- test/test_initial.py
+++ test/test_initial.py
@@ -18,4 +18,12 @@ def test():
     _ = acro.crosstab(data["year"], data["grant_type"])
     # finalise
     output: dict = acro.finalise()
-    assert output["output_0"]["outcome"] == "fail; suppression applied"
+    correct_output: str = (
+        '{"G":{"2010":"ok","2011":"ok","2012":"ok",'
+        '"2013":"ok","2014":"ok","2015":"ok"},"N":{"2010":"ok","2011":"ok",'
+        '"2012":"ok","2013":"ok","2014":"ok","2015":"ok"},"R":{"2010":"ok",'
+        '"2011":"ok","2012":"ok","2013":"ok","2014":"ok","2015":"ok"},'
+        '"R\\/G":{"2010":"threshold; ","2011":"threshold; ","2012":"threshold; ",'
+        '"2013":"threshold; ","2014":"threshold; ","2015":"threshold; "}}'
+    )
+    assert output["output_0"]["outcome"] == correct_output

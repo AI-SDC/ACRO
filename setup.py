3c03b4d820b8c4c6bd676d8c495ca29c4767bee3
Richard Preen <rpreen@gmail.com>
2022-10-12 11:03:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""Python setup script for installing ACRO."""
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="acro",
+    version="1.0.0",
+    license="MIT",
+    maintainer="Jim Smith",
+    maintainer_email="james.smith@uwe.ac.uk",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AI-SDC/ACRO",
+    packages=find_packages(),
+    package_data={"acro": ["default.yaml"]},
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "data-privacy",
+        "data-protection",
+        "privacy",
+        "privacy-tools",
+        "statistical-disclosure-control",
+    ],
+)

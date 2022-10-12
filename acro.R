ea5c59330755aeb58d8f24d83e82b8fc705c182d
Richard Preen <rpreen@gmail.com>
2022-10-12 18:36:12 +0100

    wrap R regression functions

---
+++ acro.R
@@ -0,0 +1,29 @@
+# install.packages("reticulate")
+library(reticulate)  # import Python modules
+
+acro = import("acro")
+ac = acro$ACRO()
+
+acro_lm = function(formula, data)
+{
+    "ACRO linear model"
+    model = ac$olsr(formula, data)
+    model$summary()
+}
+
+acro_glm = function(formula, data, family)
+{
+    "ACRO logit/probit model"
+    if (family == "logit") {
+      model = ac$logitr(formula, data)
+    } else {
+      model = ac$probitr(formula, data)
+    }
+    model$summary()
+}
+
+acro_finalise = function(filename)
+{
+  "Write outputs to file"
+  ac$finalise(filename)
+}
--- test.R
+++ test.R
@@ -1,26 +1,58 @@
 # install.packages("reticulate")
 # install.packages("haven")
 library(haven)  # read .dta
-library(reticulate)  # import Python modules
+source("acro.R")
 
-# load data
+#############
+# Load data #
+#############
+
 data = read_dta("data/test_data.dta")
 head(data)
+
+#################
+# Linear Models #
+#################
+
 # extract relevant columns
 df = data[, c("inc_activity", "inc_grants", "inc_donations", "total_costs")]
 # drop rows with missing values
 df = df[complete.cases(df), ]
+
 # fit linear model
 model = lm(formula="inc_activity ~ inc_grants + inc_donations + total_costs", data=df)
 summary(model)
 
-#' ACRO linear model
-acro_lm <- function(formula, data)
-{
-  acro = import("acro")
-  ac = acro$ACRO()
-  model = ac$olsr(formula, data)
-  model$summary()
-}
-
+# ACRO linear model
 acro_lm(formula="inc_activity ~ inc_grants + inc_donations + total_costs", data=df)
+
+#######################
+# Logit/Probit Models #
+#######################
+
+# extract relevant columns
+df = data[, c("survivor", "inc_activity", "inc_grants", "inc_donations", "total_costs")]
+# drop rows with missing values
+df = df[complete.cases(df), ]
+# convert survivor to numeric
+df = transform(df, survivor = as.numeric(survivor))
+
+# fit logit model
+model = glm(formula="survivor ~ inc_activity + inc_grants + inc_donations + total_costs", data=df, family = binomial(link = "logit"))
+summary(model)
+
+# ACRO logit model
+acro_glm(formula="survivor ~ inc_activity + inc_grants + inc_donations + total_costs", data=df, family="logit")
+
+# fit probit model
+model = glm(formula="survivor ~ inc_activity + inc_grants + inc_donations + total_costs", data=df, family = binomial(link = "probit"))
+summary(model)
+
+# ACRO probit model
+acro_glm(formula="survivor ~ inc_activity + inc_grants + inc_donations + total_costs", data=df, family="probit")
+
+############
+# Finalise #
+############
+
+acro_finalise("r_test.xlsx")

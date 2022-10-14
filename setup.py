e429f3fcad084b5e8582fd6c29e53ca7a78e9a77
Richard Preen <rpreen@gmail.com>
2022-10-14 15:42:09 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="acro",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     maintainer="Jim Smith",
     maintainer_email="james.smith@uwe.ac.uk",

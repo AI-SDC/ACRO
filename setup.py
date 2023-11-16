a2deb2e8f512ac472e7c3fab8fd64aa48b3f0bce
Jim-smith <jim-smith@users.noreply.github.com>
2023-11-16 10:20:30 +0000

    Update setup.py
    Signed-off-by: Jim-smith <jim-smith@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="acro",
-    version="0.4.3",
+    version="0.4.4",
     license="MIT",
     maintainer="Jim Smith",
     maintainer_email="james.smith@uwe.ac.uk",

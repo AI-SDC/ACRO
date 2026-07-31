b21e9115791d36c276acc5744b1bc609c951de1c
Jessica Ikechukwu <Jessica.Ikechukwu@uwe.ac.uk>
2026-07-31 07:01:10 +0100

    Fix formatting of docstring in tablemodeldetails.py
    Signed-off-by: Jessica Ikechukwu <Jessica.Ikechukwu@uwe.ac.uk>

--- acro/tablemodeldetails.py
+++ acro/tablemodeldetails.py
@@ -40,7 +40,7 @@ class TableModelDetails:
         thekwargs: dict | None = None,
         risk_appetite: dict | None = None,
     ) -> None:
-        """Construct the TableModelDescriptor for a table/ array type analysis.  # noqa: D212,D213,D413.
+        """Construct the TableModelDescriptor for a table/ array type analysis.
 
         Parameters
         ----------
